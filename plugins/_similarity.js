d10f702f90e91b3f519847b9d0c1d91fc5601a01
Dark Man <95143027+Dark-Man747@users.noreply.github.com>
2023-06-24 16:27:37 +0400

    Ⓒ 𝙳𝚊𝚛𝚔 𝙼𝚊𝚗 𝙸𝚗𝚌.

--- plugins/_similarity.js
+++ plugins/_similarity.js
@@ -5,21 +5,16 @@ export async function before(m, { conn, match, usedPrefix, command }) {
   if ((usedPrefix = (match[0] || '')[0])) {
     if (!m.text) return
     let noPrefix = m.text.replace(usedPrefix, '')
-    let args = noPrefix.trim().split` `.slice(1)
-    let text = args.join` `
     let help = Object.values(plugins).filter(v => v.help && !v.disabled).map(v => v.help).flat(1)
     if (help.includes(noPrefix)) return
     let mean = didyoumean(noPrefix, help)
     let sim = similarity(noPrefix, mean)
     let som = sim * 100
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let name = await conn.getName(who)
-    let caption = `
+    m.reply(`
 ربما كنت تقصد:
 
 இ ${usedPrefix + mean}
-இ التشابه: ${parseInt(som)}%`
-    if (mean) {this.sendMessage(m.chat, caption)}
+இ التشابه: ${parseInt(som)}%`)
   }
 }
 
