bd514190a9f7fd74eb8247e735289731732c6735
Dark Man <95143027+Dark-Man747@users.noreply.github.com>
2023-06-24 18:08:18 +0400

    🇴🇲

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -1,8 +1,8 @@
-export async function all(m, conn ) {
+export async function all(m) {
 // عندما يرسل شخص ما رابط مجموعة إلى رسائل الخاصة للبوت/
 if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('open this link')) && !m.isBaileys && !m.isGroup) {
 
-conn.reply(m.chat,`*دعوة البوت للانضمام إلى مجموعة*
+reply(m.chat,`*دعوة البوت للانضمام إلى مجموعة*
 
 مرحبًا @${m.sender.split('@')[0]}
 يمكنك استئجار البوت للانضمام إلى مجموعة أو التواصل مع المالك
