0c3c4e4a7254076baed24c88c867ac673f721d2b
Dark Man <95143027+Dark-Man747@users.noreply.github.com>
2023-06-24 18:06:16 +0400

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -6,7 +6,7 @@ conn.reply(m.chat,`*دعوة البوت للانضمام إلى مجموعة*
 
 مرحبًا @${m.sender.split('@')[0]}
 يمكنك استئجار البوت للانضمام إلى مجموعة أو التواصل مع المالك
-`, m, { mentions: [who] })
+`, m, { mentions: [m.sender] })
 m.react('💎')
 const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
