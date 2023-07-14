40979295b60a4426335f9bf96d3dd2cf0ea5c51f
Dark Man <95143027+Dark-Man747@users.noreply.github.com>
2023-07-14 16:55:32 +0400

    Ⓒ 𝙳𝚊𝚛𝚔 𝙼𝚊𝚗 𝙸𝚗𝚌.

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,28 +1,28 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
-  if (!text) throw `LINK?`;
+  if (!text) throw `الرابط?`;
   m.reply(wait);
 
   let res;
   try {
     res = await fetch(`https://inrl-web.onrender.com/api/insta?url=${text}`);
   } catch (error) {
-    throw `An error occurred: ${error.message}`;
+    throw `حدث خطأ: ${error.message}`;
   }
 
   let api_response = await res.json();
   if (!api_response || !api_response.result || api_response.result.length === 0) {
-    throw `No video found or Invalid response from API.`;
+    throw `لم يتم العثور على فيديو أو رد غير صالح من API.`;
   }
 
-  let cap = `HERE IS THE VIDEO >,<`;
+  let cap = `هنا الفيديو >,<`;
 
   conn.sendFile(m.chat, api_response.result[0], 'instagram.mp4', cap, m);
 }
 
 handler.help = ['instagram']
 handler.tags = ['downloader']
-handler.command = /^(instagram|igdl|ig|instagramdl)$/i
+handler.command = /^(instagram|igdl|ig|instagramdl|انستا|انستجرام|انستغرام)$/i
 
 export default handler
