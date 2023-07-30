4aa56cae14e1f664ceac2a431dd97eba767f0613
Dark Man <95143027+Dark-Man747@users.noreply.github.com>
2023-07-31 02:09:09 +0400

    Ⓒ 𝙳𝚊𝚛𝚔 𝙼𝚊𝚗 𝙸𝚗𝚌.

--- plugins/RAFF-Openai.js
+++ plugins/RAFF-Openai.js
@@ -1,1 +1,18 @@
-const _0x52530e=_0x5147;(function(_0x327760,_0x25627e){const _0x39b7e8=_0x5147,_0x2327f3=_0x327760();while(!![]){try{const _0x12c6b0=-parseInt(_0x39b7e8(0x72))/0x1+-parseInt(_0x39b7e8(0x76))/0x2*(parseInt(_0x39b7e8(0x86))/0x3)+parseInt(_0x39b7e8(0x75))/0x4+parseInt(_0x39b7e8(0x81))/0x5+parseInt(_0x39b7e8(0x82))/0x6*(-parseInt(_0x39b7e8(0x83))/0x7)+-parseInt(_0x39b7e8(0x77))/0x8*(-parseInt(_0x39b7e8(0x79))/0x9)+-parseInt(_0x39b7e8(0x80))/0xa*(parseInt(_0x39b7e8(0x84))/0xb);if(_0x12c6b0===_0x25627e)break;else _0x2327f3['push'](_0x2327f3['shift']());}catch(_0x164c73){_0x2327f3['push'](_0x2327f3['shift']());}}}(_0x1440,0xa866c));import _0x1396bb from'node-fetch';let handler=async(_0x712de2,{text:_0x5a9d66,usedPrefix:_0x39fac9,command:_0x8c3214})=>{const _0x239128=_0x5147;if(!_0x5a9d66)throw'*أدخل\x20نص\x20أو\x20طلبًا\x20لاستخدام\x20ChatGpt*\x0a\x0a*مثال:*\x0a*'+(_0x39fac9+_0x8c3214)+_0x239128(0x73)+(_0x39fac9+_0x8c3214)+'\x20اكتب\x20رمز\x20JS*';try{const _0x104606=await _0x1396bb(_0x239128(0x74)+encodeURIComponent(_0x5a9d66)),_0x1bc1df=await _0x104606['json'](),_0x18e342=_0x1bc1df['result'][_0x239128(0x88)]||_0x239128(0x7f),_0x3ad05f=_0x18e342+'\x0a\x0aCreator:\x20*dark\x20man*';_0x712de2[_0x239128(0x7d)](_0x3ad05f[_0x239128(0x7e)](_0x1bc1df));}catch(_0xecfca3){console[_0x239128(0x7c)](_0x239128(0x87),_0xecfca3);throw _0x239128(0x7b);}};function _0x1440(){const _0x24849a=['9970DKwBNF','1515845shdapM','59604rhAbks','448tBDCFj','7183YvfVHi','بوت','302394araMze','Error:','content','447124MRHEgc','\x20أحدث\x20سلسلة\x20Netflix*\x0a*','https://botz-openai-prod-botz-chatgpt-s78su6.mo1.mogenius.io/chatgpt?text=','5274100IQKCLH','2DVNifU','802504kSdPnD','command','81ZmCSpS','bro','*ERROR*','error','reply','trim','⚠\x20حدث\x20خطأ\x20داخلي\x20في\x20الخادم'];_0x1440=function(){return _0x24849a;};return _0x1440();}function _0x5147(_0x3b2289,_0x3ecc7b){const _0x1440d8=_0x1440();return _0x5147=function(_0x51472f,_0x3604df){_0x51472f=_0x51472f-0x72;let _0x568dda=_0x1440d8[_0x51472f];return _0x568dda;},_0x5147(_0x3b2289,_0x3ecc7b);}handler[_0x52530e(0x78)]=[_0x52530e(0x7a),'chatgpt','ai',_0x52530e(0x85)];export default handler;
+import fetch from 'node-fetch';
+let handler = async (m, { text, usedPrefix, command }) => {
+  if (!text) throw `*أدخل نص أو طلبًا لاستخدام ChatGpt*\n\n*مثال:*\n*${usedPrefix + command} أحدث سلسلة Netflix*\n*${usedPrefix + command} اكتب رمز JS*`;
+  try {
+    const response = await fetch(`https://botz-openai-prod-botz-chatgpt-s78su6.mo1.mogenius.io/chatgpt?text=${encodeURIComponent(text)}`);
+    const data = await response.json();
+    const result = data.result['result']
+    const fullResult = `${result}\n\nCreator: '*dark man*`;
+    m.reply(fullResult.trim(data));
+  } catch (error) {
+    console.error('Error:', error);
+    throw `*ERROR*`;
+  }
+};
+
+handler.command = ['bro', 'chatgpt', 'ai', 'بوت'];
+
+export default handler
