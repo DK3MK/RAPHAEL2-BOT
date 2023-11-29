0924798cc1c9c913b0b8b49d14884646bd6469b0
DK3MK <95143027+DK3MK@users.noreply.github.com>
2023-11-29 23:42:15 +0400

    Ⓒ 𝙳𝚊𝚛𝚔 𝙼𝚊𝚗 𝙸𝚗𝚌.

--- config.js
+++ config.js
@@ -1,72 +1,41 @@
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
-import fs from 'fs'
-import fetch from 'node-fetch'
-import axios from 'axios'
-
 
 global.owner = [
-  ['96894116692', '𝖽𝖺𝗋𝗄 𝗆𝖺𝗇', true],
+  ['96894116692', 'dark man', true],
 ] //Number of owners
 
-global.mods = ['96894116692'] 
-global.prems = ['96894116692']
-global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = ['BrunoSobrino']
+global.mods = ['96894116692','96898172380'] 
+global.prems = ['96894116692','96898172380']
+
 
 global.APIs = { // API Prefix
-  // name: 'https://website'
-  xteam: 'https://api.xteam.xyz', 
-  dzx: 'https://api.dhamzxploit.my.id',
-  lol: 'https://api.lolhuman.xyz',
-  violetics: 'https://violetics.pw',
-  neoxr: 'https://api.neoxr.my.id',
-  zenzapis: 'https://zenzapis.xyz',
-  akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',
-  nrtm: 'https://fg-nrtm.ddns.net',
-  bg: 'http://bochil.ddns.net',
   fgmods: 'https://api-fgmods.ddns.net'
 }
-global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,
-  'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`, 
-  'https://api-fgmods.ddns.net': 'fg-dylux'
+
+global.APIKeys = { // Apikey : 42x5rO7o \\
+  'https://api-fgmods.ddns.net': '42x5rO7o'
 }
 
 // Sticker WM
-global.ownername = '𝖽𝖺𝗋𝗄 𝗆𝖺𝗇'
-global.botname = 'ℝ𝔸ℙℍ𝔸𝔼𝕃'
+global.ownername = 'dark man'
+global.botname = 'رافـاييـل بــوت'
 global.premium = 'false'
-global.packname = 'ℝ𝔸ℙℍ𝔸𝔼𝕃 ┃ ＢＯＴ ²⁰²³'
-global.author = '𝖽𝖺𝗋𝗄 𝗆𝖺𝗇'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/aboud_coding\n'
-global.dygp = 'https://chat.whatsapp.com'
-global.fgsc = 'https://github.com/Dark-Man747/RAPHAEL-BOT' 
-global.fgyt = 'https://youtube.com/@9TL'
-global.fgpyp = 'https://youtube.com/@9TL'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
-global.dbase = 'mongodb+srv://action20005:@123456aa@bot-whatsapp.3owz610.mongodb.net/?retryWrites=true&w=majority' //ADD YOUR MONGODB BY CHANGIMG IT
-//mongodb+srv://action20005:@123456aa@bot-whatsapp.3owz610.mongodb.net/?retryWrites=true&w=majority
-global.wait = '*جارٍ التحميل، يرجى الانتظار....*'
+global.packname = 'رافـاييـل بــوت'
+global.author = 'Dev >> dark man'
+
+
+global.wait = '*[■■■■■■■■■□] 90%*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
 global.error = '❌'
 global.xmoji = '🔥'
 
-global.multiplier = 70 
-global.maxwarn = '5' // máxima advertencias //
+
+global.multiplier = 69 
+global.maxwarn = '3' // máxima advertencias //
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
